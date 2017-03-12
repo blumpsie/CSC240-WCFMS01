12a887138ad6336988191b9a4c14fe5dab5a180b
Mark Erickson <blumpsie@gmailcom>
2017-03-12 18:25:41 -0400

    Menus!!!! Menus!!!!!! Menus!!!!
    Implemented the menu options.

--- CSC240-WCFMS01/Program.cs
+++ CSC240-WCFMS01/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CSC240_WCFMS01
@@ -57,11 +57,61 @@ namespace CSC240_WCFMS01
             // Sort the array of Movie objects
             bubbleSort(theMovies);
 
-            // temp display
-            foreach (Movie movie in theMovies)
+            // present the menu for user to choose from
+            // and process their choice
+            bool terminate = false;
+            do
             {
-                movie.display();
-            }
+                bool validChoice = false;
+                int userChoice = 0;
+                do
+                {
+                    Console.Write("\nWEST CHESTER FABULOUS MOVIE SOCIETY DATA MENU\n"
+                                 + "1 - Display the titles of all the movies\n"
+                                 + "2 - Display the data for a specific movie\n"
+                                 + "3 - Display the titles of all the movies released in a given year\n"
+                                 + "4 - Display the titles of all the movies with a particular star\n"
+                                 + "5 - Quit the program\n");
+                    userChoice = Convert.ToInt32(Console.ReadLine());
+
+                    if (userChoice != 1 ||
+                        userChoice != 2 ||
+                        userChoice != 3 ||
+                        userChoice != 4 ||
+                        userChoice != 5)
+                    {
+                        validChoice = true;
+                    }
+
+                } while (!validChoice);
+
+                switch (userChoice)
+                {
+                    case 1:
+                        Console.WriteLine("NOT IMPLEMENTED YET!!!!");
+                        break;
+                    case 2:
+                        Console.WriteLine("NOT IMPLEMENTED YET!!!!");
+                        break;
+                    case 3:
+                        Console.WriteLine("NOT IMPLEMENTED YET!!!!");
+                        break;
+                    case 4:
+                        Console.WriteLine("NOT IMPLEMENTED YET!!!!");
+                        break;
+                    case 5:
+                        Console.WriteLine("\nAre you sure (Y/N)?");
+                        string exitChoice = Console.ReadLine().ToUpper();
+                        if (exitChoice[0] == 'Y')
+                        {
+                            terminate = true;
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\nNot a valid choice. Please choose again.");
+                        break;
+                }
+            } while (!terminate);
         }
 
         // sorts an array of Movie objects using bubble sort
