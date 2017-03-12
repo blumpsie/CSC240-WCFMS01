05d07ae3c0e20e70c32635c17bc0da814b94a343
Mark Erickson <blumpsie@gmailcom>
2017-03-12 14:01:36 -0400

    initial commit
    Created the project and made the Movie class.

---
+++ CSC240-WCFMS01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC240_WCFMS01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie aMovie = new Movie();
+            aMovie.readIn();
+            aMovie.display();
+        }
+    }
+}
